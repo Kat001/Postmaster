7f9c0c42cce3dc5cb4153f163ff4b37b8128f649
“Kat001” <devdivakar8@gmail.com>
2021-02-13 11:52:12 +0530

    eroor solved

--- lib/main.dart
+++ lib/main.dart
@@ -78,24 +78,3 @@ class _MyappState extends State<Myapp> {
     );
   }
 }
-  }*/
-}
-
-/*// Widget build(BuildContext context) {
-//     return LayoutBuilder(                           //return LayoutBuilder
-//       builder: (context, constraints) {
-//         return OrientationBuilder(                  //return OrientationBuilder
-//           builder: (context, orientation) {
-//             //initialize SizerUtil()
-//             SizerUtil().init(constraints, orientation);  //initialize SizerUtil
-//             return MaterialApp(
-//               debugShowCheckedModeBanner: false,
-//               title: "Login",
-//               theme: ThemeData(primaryColor: Color(0xFF27DEBF)),
-//               home: Homepage(),
-//             );
-//           },
-//         );
-//       },
-//     );
-//   }*/
